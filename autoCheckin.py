af0df4cd03ee01b95f53a966a00423aec08165e4
Shennoter <36178491+Shennoter@users.noreply.github.com>
2021-02-01 00:46:59 +0800

    Add files via upload

---
+++ autoCheckin.py
@@ -0,0 +1,27 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from info import *
+
+
+def sign_in(uid, pwd):
+    driver_url = r"C:\Users\shenn\source\repos\一键打卡\msedgedriver.exe"
+    driver = webdriver.Edge(executable_path=driver_url)
+    driver.get("https://stuhealth.jnu.edu.cn/#/login")
+    # 填入学号密码
+    driver.find_element_by_id("zh").send_keys(uid)
+    driver.find_element_by_id("passw").send_keys(pwd)
+    # 提交
+    driver.find_element_by_css_selector('[type=submit]').click()
+    # 等待页面加载完成
+    time.sleep(5)
+    driver.find_element_by_id('10000').click()
+    driver.find_element_by_id('tj').click()
+    # 另一种元素定位方式
+    #driver.find_element_by_css_selector('[type=checkbox]').click()
+    #driver.find_element_by_css_selector('[type=submit]').click()
+    driver.close()
+
+
+if __name__ == "__main__":
+    sign_in(UID, PWD)
